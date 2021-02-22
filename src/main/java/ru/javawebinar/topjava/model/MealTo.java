3824b47cb9efd180a70d73da27a4952c2a90191b
Oleg Mironenko <atek@ngs.ru>
2021-02-22 23:42:37 +0700

    HW1 (22) 1

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -3,6 +3,8 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDateTime;
 
 public class MealTo {
+
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,11 +13,17 @@ public class MealTo {
 
     private final boolean excess;
 
+
+
+
     public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
+
+
+
     }
 
     @Override
