c8950c3b48bb6a11491bf0c09de2048b51b6e12c
Oleg Mironenko <atek@ngs.ru>
2021-02-24 17:53:00 +0700

    HW1 (22) 5

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -21,10 +21,27 @@ public class MealTo {
         this.description = description;
         this.calories = calories;
         this.excess = excess;
-
-
-
     }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public boolean isExcess() {
+        return excess;
+    }
+
+
+
+
 
     @Override
     public String toString() {
